e109802872475a02414a0e85efac44d8b1ee9939
Josephsan-2011 <83101904+Josephsan-2011@users.noreply.github.com>
2022-01-19 18:25:15 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//https://teachablemachine.withgoogle.com/models/l6B8qc6Id/
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'jpg',
+    jpg_quality: 90
+});
+var camera = document.getElementById("camera")
+Webcam.attach("#camera")
+
+function snapshot() {
+    Webcam.snap(function (data_URI) {
+        document.getElementById("result").innerHTML = '<img id = "capture_image" src="' + data_URI + '"/>'
+    })
+}
+console.log(ml5.version)
+var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/l6B8qc6Id/model.json',model_loaded)
+function model_loaded() {
+    console.log('model_loaded')
+}
+function check() {
+    img = document.getElementById('capture_image')
+    classifier.classify(img,gotresult)
+}
+function gotresult(error,results) {
+    if (error) {
+        console.error(error)
+    }
+    else {
+        console.log(results)
+        document.getElementById("object_name").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
+}
